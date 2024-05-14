f2a8307b5e782b15a7445daccb8d08ff605e10c2
Thiago Castilho <tdecasti@my.centennialcollege.ca>
2024-05-14 15:56:13 -0400

    using vite to deploy

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+import Home from './components/home';
+import About from './components/about'
+
+function App() {
+  return (
+    <>
+      <Home />
+      <About />
+    </>
+  )
+}
+
+export default App
